8ed05799780eaedff56fffe9f819372effccfb20
David Cole <misterdavemeister@gmail.com>
2015-12-20 10:11:49 -0600

    added alertify for custom confirm alerts

--- public/app/admin/mvUserDetailCtrl.js
+++ public/app/admin/mvUserDetailCtrl.js
@@ -1,16 +1,24 @@
-angular.module('app').controller('mvUserDetailCtrl', function($scope, mvCachedUsers, $routeParams, mvUser, $location, mvNotifier) {
+angular.module('app').controller('mvUserDetailCtrl', function($scope, mvCachedUsers, $routeParams, mvUser, $location, mvNotifier, alertify) {
   mvCachedUsers.query().$promise.then(function(collection) {
     collection.forEach(function(user) {
       if (user._id === $routeParams.id) {
         $scope.user = user;
+        console.log(user);
       }
     });
   });
   $scope.deleteUser = function(id, firstName, lastName) {
-     mvUser.delete({_id: id}, function() {
-       $scope.users = mvCachedUsers.reload();
-       $location.path("/admin/users");
-       mvNotifier.success("You have successfully deleted the user '" + firstName + " " + lastName + "'!");
-     });
+    alertify
+      .okBtn("Yes")
+      .cancelBtn("No")
+      .confirm("Delete " + firstName + " " + lastName + "?", function () {
+      mvUser.delete({_id: id}, function () {
+        $scope.users = mvCachedUsers.reload();
+        $location.path("/admin/users");
+        mvNotifier.success("You have successfully deleted the user '" + firstName + " " + lastName + "'!");
+        alertify
+          .reset();
+      });
+    });
   };
 });
