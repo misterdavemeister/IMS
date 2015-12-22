28ec573abbc4a0d10eb7ff71222287ce3cd2b439
David Cole <misterdavemeister@gmail.com>
2015-12-21 22:29:01 -0600

    added search and sort for admin user page

--- public/app/admin/mvUserListCtrl.js
+++ public/app/admin/mvUserListCtrl.js
@@ -1,3 +1,9 @@
 angular.module('app').controller('mvUserListCtrl', function($scope, mvCachedUsers) {
    $scope.users = mvCachedUsers.query();
+
+   $scope.sortOptions = [{value: 'firstName', text: 'Sort by First Name'},
+      {value: 'lastName', text: 'Sort by Last Name' },
+      {value: 'role', text: 'Sort by Role'}];
+
+   $scope.sortOrder = $scope.sortOptions[0].value;
 });
