6c940d1cf33618939d158fda25c70847052e2cb1
David Cole <misterdavemeister@gmail.com>
2015-12-26 23:08:43 -0600

    changed products page

--- public/app/screens/products/mvProductsScreenCtrl.js
+++ public/app/screens/products/mvProductsScreenCtrl.js
@@ -1,5 +1,10 @@
 angular.module('app').controller('mvProductsScreenCtrl', function($scope, mvCachedProduct, mvIdentity) {
+  $scope.isHovering = false;
   $scope.products = mvCachedProduct.query();
   $scope.name = "mvProductsScreenCtrl";
   $scope.identity = mvIdentity;
+  $scope.showButton = function() {
+    console.log("hovering");
+    $scope.isHovering = true;
+  };
 });
