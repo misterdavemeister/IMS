3b2edc47cbfc4229a073a23daf6b340cbcba1ded
David Cole <misterdavemeister@gmail.com>
2015-12-27 01:16:26 -0600

    added filters and search to products page / price is type:Number instead of string

--- public/app/screens/products/mvProductsScreenCtrl.js
+++ public/app/screens/products/mvProductsScreenCtrl.js
@@ -1,10 +1,14 @@
 angular.module('app').controller('mvProductsScreenCtrl', function($scope, mvCachedProduct, mvIdentity) {
-  $scope.isHovering = false;
   $scope.products = mvCachedProduct.query();
-  $scope.name = "mvProductsScreenCtrl";
   $scope.identity = mvIdentity;
-  $scope.showButton = function() {
-    console.log("hovering");
-    $scope.isHovering = true;
-  };
+
+  $scope.sortOptions = [{value:'name', text:'Sort by Product Name'},
+    {value:'description', text:'Sort by Product Description'},
+    {value:'upc', text:'Sort by Product UPC'},
+    {value:'product_id', text:'Sort by Product ID'},
+    {value:'quantity', text:'Sort by Quantity'},
+    {value:'price', text:'Sort by Price'},
+    {value:'manufacturer', text:'Sort by Manufacturer'}
+  ];
+  $scope.sortOrder = $scope.sortOptions[0].value;
 });
