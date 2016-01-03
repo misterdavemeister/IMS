33c28d67ef4d39e1bdbf0898a73c015fe04dadae
David Cole <misterdavemeister@gmail.com>
2016-01-03 09:28:45 -0600

    added temporary link to new purchase order page

--- public/app/main/controllers/mvMainCtrl.js
+++ public/app/main/controllers/mvMainCtrl.js
@@ -1,3 +1,10 @@
 angular.module('app').controller('mvMainCtrl', function($scope, mvIdentity) {
   $scope.identity = mvIdentity;
+
+  //for screens dropdown menu
+
+  $scope.showInbound = false;
+  $scope.toggleInbound = function() {
+    $scope.showInbound = !$scope.showInbound;
+  }
 });
