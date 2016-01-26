955f765ed7e0006d3ea3438ae0ded29c9b84021b
David Cole <misterdavemeister@gmail.com>
2016-01-26 17:51:04 -0600

    fixed routing issue with outbound orders

--- server/controllers/outboundOrders.js
+++ server/controllers/outboundOrders.js
@@ -2,11 +2,20 @@ var OutboundOrder = require('mongoose').model('OutboundOrder');
 
 exports.getOutboundOrders = function(req, res) {
   OutboundOrder.find({})
-               .exec(function(err, collection) {
+               .exec(function (err, collection) {
                  if (err) {
                    res.statusCode(400);
-                   res.send({reason:err.toString()});
+                   res.send({reason: err.toString()});
                  }
                  res.send(collection);
+               });
+};
+exports.getOrderById = function(req, res) {
+  OutboundOrder.findOne({_id: req.params.id}, function(err) {
+    if (err) {
+      res.sendStatus(400);
+      res.send({reason: err.toString()});
+    }
+    res.sendStatus(200);
   });
 };
